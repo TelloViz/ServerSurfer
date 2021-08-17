a207515f7297d9c8a637ec0befe00d65f6369e9e
Tello- <tellovisionstudio@gmail.com>
2021-08-17 16:10:29 -0700

    Buttons now transition states
    Main Menu Start goes to Playing, Main Menu Quit goes to Quitting

--- Assets/Scripts/QuitButtonResponse.cs
+++ Assets/Scripts/QuitButtonResponse.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vast.StateMachine;
 
 public class QuitButtonResponse : MonoBehaviour
 {
+    [SerializeField] private AppManager appManager;
+    [SerializeField] private State responseState;
     public void Respond()
     {
-        Debug.Log("Quitbutton responded to");
+        Debug.Log("QuitButton responded to");
+        if (appManager.AppStateMachine.ContainsState(responseState))
+        {
+            appManager.AppStateMachine.ChangeState(responseState);
+        }
     }
 }
--- Assets/Scripts/StartButtonResponse.cs
+++ Assets/Scripts/StartButtonResponse.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vast.StateMachine;
 
 public class StartButtonResponse : MonoBehaviour
 {
+    [SerializeField] private AppManager appManager;
+    [SerializeField] private State responseState;
     public void Respond()
     {
         Debug.Log("StartButton responded to");
+        if(appManager.AppStateMachine.ContainsState(responseState))
+        {
+            appManager.AppStateMachine.ChangeState(responseState);
+        }
     }
 }
