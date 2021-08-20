0295e1369f93a55164f96f410cb224ca1f75ad22
Tello- <tellovisionstudio@gmail.com>
2021-08-20 13:31:28 -0700

    Plugged Unity UI Demo into my project
    Works great, transitions just as before. Looks nicer

---
+++ Assets/Scripts/GameEventRaiser.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventRaiser : MonoBehaviour
+{
+    [SerializeField] AppManager appManager;
+    [SerializeField] GameEvent startButtonClickedEvent;
+    [SerializeField] GameEvent quitButtonClickedEvent;
+
+    public AppManager AppManager { get => appManager; set => appManager = value; }
+
+    public void RaiseStartButtonClickedEvent()
+    {
+        startButtonClickedEvent.Raise();
+    }
+    public void RaiseQuitButtonClickedEvent()
+    {
+        quitButtonClickedEvent.Raise();
+    }
+}
