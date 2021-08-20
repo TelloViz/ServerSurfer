d75f933820d5c8523814438d9b222546493b2879
Tello- <tellovisionstudio@gmail.com>
2021-08-20 16:11:18 -0700

    Added a header comment to Unity's AppManager file

--- Assets/Unity UI Samples/Scripts/ApplicationManager.cs
+++ Assets/Unity UI Samples/Scripts/ApplicationManager.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/* This file was implemented by unity as part of their Demo GUI Menu project.
+ 
+ I interpret this as being a file that handles things on the application level.
+The only task they are doing in this file so far is exiting the application. 
+They do a simple check for editor window or runtime game mode...*/
+
+using UnityEngine;
 using System.Collections;
 
 public class ApplicationManager : MonoBehaviour {
